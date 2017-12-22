df88746e0d1dbf0980b43f6b470ea3291b138345
Roland Meertens <rolandmeertens@gmail.com>
2017-12-22 12:43:40 +0100

    added tests

---
+++ runtestscripts.sh
@@ -0,0 +1,8 @@
+
+jupyter nbconvert --ExecutePreprocessor.timeout=6000 --to notebook --execute /notebooks/Section\ 1.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=6000 --to notebook --execute /notebooks/Section\ 2\ Tensorboard.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=6000 --to notebook --execute /notebooks/Section\ 3\ autoencoders.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=6000 --to notebook --execute /notebooks/Section\ 4\ Siamese\ networks.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=6000 --to notebook --execute /notebooks/Section\ 5\ atari\ video.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=6000 --to notebook --execute /notebooks/Section\ 5\ openai\ gym.ipynb
+
---
+++ test.sh
@@ -0,0 +1,2 @@
+docker-compose build
+docker-compose -f docker-compose.yml.test up
